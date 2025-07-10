f54f8055b20a95ffaebe9d6f207f9fc1298de1cb
Faiz Khan <k4621694@gmail.com>
2025-07-10 20:28:31 +0530

    Add Vercel Analytics and update .gitignore for package-lock.json

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
+import { Analytics } from "@vercel/analytics/react";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Navigation from "@/components/Navigation";
@@ -40,6 +41,7 @@ function App() {
             <Router />
           </main>
           <Toaster />
+          <Analytics />
         </div>
       </TooltipProvider>
     </QueryClientProvider>
